f7677a33df696dc2e81ad07ccec6683744c8ff80
colecrum26 <cwcrumrine@gmail.com>
2023-08-29 15:21:05 -0500

    purging boilerplate

---
+++ portfolio2/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav></nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
